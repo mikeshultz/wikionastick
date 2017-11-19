bd4f1b506ccb7a14f2342edeb987d2d206f250b8
Mike Shultz <shultzm@gmail.com>
2017-11-18 23:02:12 -0700

    Coordinating between travis and make.sh with env vars

--- make.sh
+++ make.sh
@@ -4,41 +4,46 @@
 VERSION="0.1.1"
 
 CWD=$(pwd)
-BUILD_DIR="$CWD/build"
+# Set build directory only if it hasn't already been set
+[ -z "$WOS_BUILD_DIR" ] && WOS_BUILD_DIR="$CWD/build"
 
 # Build directories, filenames, and other per-arch settings
 
 # Windows
-WIN64_DIR="$BUILD_DIR/win64"
+WIN64_DIR="$WOS_BUILD_DIR/win64"
 WIN64="$WIN64_DIR/wiki.exe"
 WIN64_TARBALL="wikionastick-$VERSION-windows64.tar.gz"
 
 # Linux
-LINUX64_DIR="$BUILD_DIR/linux64"
+LINUX64_DIR="$WOS_BUILD_DIR/linux64"
 LINUX64="$LINUX64_DIR/wiki.linux64"
 LINUX64_TARBALL="wikionastick-$VERSION-linux64.tar.gz"
 
 # FreeBSD
-FBSD64_DIR="$BUILD_DIR/freebsd64"
+FBSD64_DIR="$WOS_BUILD_DIR/freebsd64"
 FBSD64="$FBSD64_DIR/wiki.freebsd64"
 FBSD64_TARBALL="wikionastick-$VERSION-freebsd64.tar.gz"
 
 # Darwin
-DARWIN_DIR="$BUILD_DIR/darwin64"
+DARWIN_DIR="$WOS_BUILD_DIR/darwin64"
 DARWIN="$DARWIN_DIR/wiki.app"
 DARWIN_TARBALL="wikionastick-$VERSION-darwin64.tar.gz"
 
 # Combined
-COMBINED_DIR="$BUILD_DIR/combined"
+COMBINED_DIR="$WOS_BUILD_DIR/combined"
 COMBINED_TARBALL="wikionastick-$VERSION-all.tar.gz"
 
+echo "WikiOnAStick v$VERSION Build Script"
+echo "==================================="
+echo "Build directory: $WOS_BUILD_DIR"
+echo ""
 echo "go getting the dependencies..."
 
 go get -t github.com/mikeshultz/wikionastick
 
 echo "Creating build environment..."
 
-rm -rf $BUILD_DIR/*
+rm -rf $WOS_BUILD_DIR/*
 
 # Starting with the templates dir because fewer commands
 mkdir -p $LINUX64_DIR/templates
@@ -69,10 +74,10 @@ cp -R templates $COMBINED_DIR
 
 echo "Generating tarballs..."
 
-cd $WIN64_DIR && tar -czf $BUILD_DIR/$WIN64_TARBALL . && cd $CWD
-cd $LINUX64_DIR && tar -czf $BUILD_DIR/$LINUX64_TARBALL . && cd $CWD
-cd $FBSD64_DIR && tar -czf $BUILD_DIR/$FBSD64_TARBALL . && cd $CWD
-cd $DARWIN_DIR && tar -czf $BUILD_DIR/$DARWIN_TARBALL . && cd $CWD
-cd $COMBINED_DIR && cp ../*/wiki.* $COMBINED_DIR/ && tar -czf $BUILD_DIR/$COMBINED_TARBALL . && cd $CWD
+cd $WIN64_DIR && tar -czf $WOS_BUILD_DIR/$WIN64_TARBALL . && cd $CWD
+cd $LINUX64_DIR && tar -czf $WOS_BUILD_DIR/$LINUX64_TARBALL . && cd $CWD
+cd $FBSD64_DIR && tar -czf $WOS_BUILD_DIR/$FBSD64_TARBALL . && cd $CWD
+cd $DARWIN_DIR && tar -czf $WOS_BUILD_DIR/$DARWIN_TARBALL . && cd $CWD
+cd $COMBINED_DIR && cp ../*/wiki.* $COMBINED_DIR/ && tar -czf $WOS_BUILD_DIR/$COMBINED_TARBALL . && cd $CWD
 
 echo "Done."
